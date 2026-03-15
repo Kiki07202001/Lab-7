31b138d622e2d46c6a75c460a310413d2564b161
Kiki Brusey <kikibrusey@Kikis-MacBook-Air.local>
2026-03-14 22:55:14 -0400

    Added keyboard event. Wrapped events in Dom

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-// Mouse event
-const button = document.getElementById("hoverButton");
-const message = document.getElementById("mouseMessage");
+document.addEventListener("DOMContentLoaded", function() {
 
-button.addEventListener("mouseenter", function() {
-    message.textContent = 'Wow, its so warm';
-}
-);
+    // MOUSE EVENT
 
-button.addEventListener("mouseleave", function() {
-    message.textContent = 'Wait...Come back';
-}
-);
+    const button = document.getElementById("hoverButton");
+    const mouseMessage = document.getElementById("mouseMessage");
+
+    button.addEventListener("mouseenter", function() {
+        mouseMessage.textContent = 'Wow, its so warm';
+    });
+
+    button.addEventListener("mouseleave", function() {
+        mouseMessage.textContent = 'Wait... come back';
+    });
+
+
+    // KEYBOARD EVENT
+
+    const input = document.getElementById("textInput");
+    const keyMessage = document.getElementById("keyMessage");
+
+    input.addEventListener("keydown", function(event) {
+        keyMessage.textContent = 'Last key pressed: ' + event.key;
+    });
+
+});
+
